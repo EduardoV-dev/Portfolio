4dd027db755cd7b4e9cc03bc8cd5bf1d990973f3
Eduardo Varela <eduardovarela139@gmail.com>
2022-01-04 06:32:08 -0600

    Initial version

--- containers/Skills/List/index.tsx
+++ containers/Skills/List/index.tsx
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
+import { animated, useTrail } from 'react-spring';
 import { designTechnologies, webDevelopmentTechnologies } from '../../../consts';
-import { Container } from '../../../components';
+import { Waypoint } from 'react-waypoint';
 import styles from './list.module.scss';
 
 interface ISkillsList {
@@ -10,20 +11,35 @@ interface ISkillsList {
 const SkillsList: React.FC<ISkillsList> = ({
   elements,
 }): JSX.Element => {
+  const [animate, setAnimate] = useState<boolean>(false);
   const skills = elements === 'web' ? webDevelopmentTechnologies : designTechnologies;
+  const trail = useTrail(skills.length, {
+    from: { opacity: 0, transform: 'translate(50%, 50%) scale(1.5) rotate(180deg)' },
+    to: { opacity: 1, transform: 'translate(0%, 0%) scale(1) rotate(0deg)' },
+    config: {
+      duration: 100,
+      bounce: 1,
+    },
+    reset: true,
+  });
 
   return (
-    <Container
-      containerType="container"
-      transparent="true"
-      className={styles.container}
+    <Waypoint
+      onEnter={() => setAnimate(true)}
+      onLeave={() => setAnimate(false)}
     >
-      {skills.map((Tech, idx) => (
-        <Fragment key={idx}>
-          {Tech}
-        </Fragment>
-      ))}
-    </Container>
+      <div
+        className={styles.container}
+      >
+        {animate
+          && trail.map((styles, idx) => (
+            <animated.div style={styles} key={idx}>
+              {skills[idx]}
+            </animated.div>
+          ))
+        }
+      </div>
+    </Waypoint>
   );
 }
 

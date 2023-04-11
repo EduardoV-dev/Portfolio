84a7b51b0c11458c52b3e33f6419d5b77937ac08
Eduardo Varela <eduardovarela139@gmail.com>
2023-04-11 17:55:35 -0600

    New changes

--- containers/AboutMe/Right/index.tsx
+++ containers/AboutMe/Right/index.tsx
@@ -4,25 +4,29 @@ import { Button, Container, Paragraph } from '../../../components';
 import styles from './right.module.scss';
 
 const AboutRight: React.FC<{}> = (): JSX.Element => {
-  return (
-    <Container containerType="container" transparent="true">
-      <Paragraph className={styles.container__about}>
-        Web developer with extensive knowledge, working with web technologies
-        and UI/UX Design in order to deliver quality work.
-      </Paragraph>
-      <div>
-        <Button
-          type="link"
-          color="primary"
-          icon={Download}
-          text="Download resume"
-          className={styles.container__btn}
-          href="/Resume.pdf"
-          download
-        />
-      </div>
-    </Container>
-  );
+    return (
+        <Container containerType="container" transparent="true">
+            <Paragraph className={styles.container__about}>
+                Web developer with extensive knowledge, working with web
+                technologies and UI/UX Design in order to deliver quality work.
+            </Paragraph>
+
+            <Paragraph className={styles.container__about}>
+                New revamped version for my portfolio is coming soon!
+            </Paragraph>
+            <div>
+                <Button
+                    type="link"
+                    color="primary"
+                    icon={Download}
+                    text="Download resume"
+                    className={styles.container__btn}
+                    href="/Resume.pdf"
+                    download
+                />
+            </div>
+        </Container>
+    );
 };
 
 export default AboutRight;
--- containers/Skills/index.tsx
+++ containers/Skills/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Base } from '../../components';
+import { Base, Paragraph } from '../../components';
 import SkillsLeft from './Left';
 import SkillsRight from './Right';
 import styles from './skills.module.scss';
 
 const Skills: React.FC<{}> = (): JSX.Element => {
-  const placeholder = { text: 'Skills', className: styles.placeholder };
+    const placeholder = { text: 'Skills', className: styles.placeholder };
 
-  return (
-    <Base
-      leftContent={SkillsLeft}
-      rightContent={SkillsRight}
-      order="pair"
-      title="Skills"
-      subtitle="Technologies I work with"
-      placeholder={placeholder}
-      id="Skills"
-    />
-  );
-}
+    return (
+        <>
+            <Base
+                leftContent={SkillsLeft}
+                rightContent={SkillsRight}
+                order="pair"
+                title="Skills"
+                subtitle="Technologies I work with"
+                placeholder={placeholder}
+                id="Skills"
+            />
+
+            <Paragraph textAlign="center" className={styles['many-more-text']}>
+                And many more...
+            </Paragraph>
+        </>
+    );
+};
 
 export default Skills;

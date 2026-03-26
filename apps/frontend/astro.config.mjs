e598a97733ca0bac670fc5b3e789806a8a179098
Eduardo Varela <eduardovarela139@gmail.com>
2026-03-25 19:53:52 -0600

    chore: added configuration for astro

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -6,4 +6,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
+    site: process.env.SITE_URL,
+    build: {
+        format: "directory",
+    },
 });

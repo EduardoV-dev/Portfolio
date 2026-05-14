31e05284922fdeff14b1eacd64b65c7e9d52c5d0
Eduardo Varela <eduardovarela139@gmail.com>
2026-05-14 12:30:20 -0600

    chore: stop tracking amplify build artifacts

--- .amplify-hosting/static/_astro/jsx-runtime.BGiF56w7.js
+++
@@ -1,9 +0,0 @@
-(function(){try{var r=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};r.SENTRY_RELEASE={id:"51a973bd64882fbe40a2b1fa1d425937ebce5721"};var a=new r.Error().stack;a&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[a]="6aa5acce-bbae-4352-a6aa-9212ad4bd577",r._sentryDebugIdIdentifier="sentry-dbid-6aa5acce-bbae-4352-a6aa-9212ad4bd577")}catch{}})();var i={exports:{}},n={};/**
- * @license React
- * react-jsx-runtime.production.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var o;function x(){if(o)return n;o=1;var r=Symbol.for("react.transitional.element"),a=Symbol.for("react.fragment");function s(l,e,t){var d=null;if(t!==void 0&&(d=""+t),e.key!==void 0&&(d=""+e.key),"key"in e){t={};for(var u in e)u!=="key"&&(t[u]=e[u])}else t=e;return e=t.ref,{$$typeof:r,type:l,key:d,ref:e!==void 0?e:null,props:t}}return n.Fragment=a,n.jsx=s,n.jsxs=s,n}var f;function R(){return f||(f=1,i.exports=x()),i.exports}var b=R();export{b as j};
